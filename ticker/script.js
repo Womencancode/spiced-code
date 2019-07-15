58586093a260a6e2f1d04c46fec627a2b1f98cea
Markus Heine <heine.m@gmxde>
2019-07-15 18:08:45 +0200

    add ticker

---
+++ ticker/script.js
@@ -0,0 +1,50 @@
+// ticker top
+
+(function() {
+    var headLines = document.getElementById("headLines");
+    var links = headLines.getElementsByTagName("a");
+    var left = headLines.offsetLeft;
+    moveHeadlines();
+    function moveHeadlines() {
+        left--;
+        if (left <= -links[0].offsetWidth) {
+            // add to left the width of the first link
+            left = left + links[0].offsetWidth;
+            // remove the first link and make it last link
+            var line = links[0].parentNode;
+            line.appendChild(links[0]);
+        }
+        // move headlines to new position
+        // console.log(left);
+        headLines.style.left = left + "px";
+
+        requestAnimationFrame(moveHeadlines);
+    }
+})();
+
+// ticker bottom
+
+(function() {
+    var bheadLines = document.getElementById("headLines_bottom");
+    var blinks = bheadLines.getElementsByTagName("a");
+    var right = bheadLines.offsetLeft + headLines.offsetWidth;
+    var blinkArrLength = blinks.length - 1;
+    moveHeadlines();
+    function moveHeadlines() {
+        right--;
+        if (right <= -blinks[blinkArrLength].offsetWidth) {
+            right = right + blinks[blinkArrLength].offsetWidth;
+            var lastLink = blinks[blinkArrLength];
+            blinks[blinkArrLength] = bheadLines.insertBefore(
+                lastLink,
+                blinks[0]
+            );
+        }
+
+        // move headlines to new position
+        // console.log(left);
+        bheadLines.style.right = right + "px";
+        // console.log(links[9].offsetWidth);
+        requestAnimationFrame(moveHeadlines);
+    }
+})();
