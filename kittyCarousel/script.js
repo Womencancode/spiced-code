e7d1a08c8027a85314c7fa5f97cab8c07a64384f
Markus Heine <heine.m@gmxde>
2019-07-18 14:36:01 +0200

    add kitty carousel

---
+++ kittyCarousel/script.js
@@ -0,0 +1,34 @@
+(function() {
+    var kitties = document.getElementsByClassName("kitty");
+    var current = 0;
+    setTimeout(moveKitty, 2000);
+
+    document.addEventListener("transitionend", function fn(e) {
+        if (e.target.classList.contains("exit")) {
+            e.target.classList.remove("exit");
+            setTimeout(moveKitty, 1000);
+        }
+    });
+
+    function moveKitty() {
+        // kitties[current]
+        //remove onscreen and add exitclasses zu the current kitty
+        console.log("The current kitty is " + current);
+        kitties[current].classList.remove("onscreen");
+        kitties[current].classList.add("exit");
+
+        // kitties[current].addEventListener("transitionend", function fn(e) {
+        //     e.target.classList.remove("exit");
+        //     e.target.removeEventListener("transitionend", fn);
+        // });
+
+        current++;
+        if (current >= kitties.length) {
+            current = 0;
+        }
+
+        //add onscreen class to next one
+        console.log("The new kitty is " + current);
+        kitties[current].classList.add("onscreen");
+    }
+})();
