973032b5ce2279af25bd6f6c076a2e649f4c7b51
Markus Heine <heine.m@gmxde>
2019-07-18 11:51:19 +0200

    delete files

--- js_exercise/20190716/e1/script.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var box = document.getElementById("box");
-
-    document.addEventListener("mousemove", function(evt) {
-        box.style.left = evt.clientX - 50 + "px";
-        box.style.top = evt.clientY - 50 + "px";
-    });
-})();
