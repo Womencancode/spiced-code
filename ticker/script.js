3e2b55c2f777b455cf019210d9c01387d42770d2
Markus Heine <heine.m@gmxde>
2019-07-22 17:06:59 +0200

    change sth

--- ticker/script.js
+++ ticker/script.js
@@ -5,13 +5,9 @@
     var links = headlines.find("a");
     var left = headlines.offset().left;
     var myReq;
-    // console.log(headLines.getElementsByTagName("a"));
-    // console.log(headLines.getElementsByTagName("A"));
     moveHeadlines();
     function moveHeadlines() {
         left--;
-        // console.log(left);
-        // console.log(links.eq(0).outerWidth());
         if (
             left <=
             -headlines
@@ -20,19 +16,14 @@
                 .outerWidth()
         ) {
             console.log("lalala");
-            // add to left the width of the first link
             left =
                 left +
                 headlines
                     .find("a")
                     .eq(0)
                     .outerWidth();
-            // remove the first link and make it last link
-            // console.log(links[0].parentNode);
             headlines.append(headlines.find("a").eq(0));
         }
-        // move headlines to new position
-        // console.log(left);
         headlines.css({
             left: left + "px"
         });
