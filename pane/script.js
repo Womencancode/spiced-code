0730f84c1a9a9a988c7874fbe157801b313db93e
Markus Heine <heine.m@gmxde>
2019-07-23 09:14:41 +0200

    change the pointer

--- pane/script.js
+++ pane/script.js
@@ -7,9 +7,12 @@ var containerOffset = animationArea.offset().left;
 
 bar.on("mousedown", function(e) {
     startPoint = e.pageX;
-    console.log(startPoint);
+    console.log("pageX: " + e.pageX);
+    console.log("screenX: " + e.screenX);
+    console.log("clientX: " + e.clientX);
     animationArea
         .on("mousemove.mover", function(e) {
+            console.log(e);
             actualPoint = e.pageX;
             barPosition = actualPoint;
             console.log(barPosition);
