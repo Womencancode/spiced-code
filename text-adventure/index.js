6de98e32d08da137dfce44ad26b16f82ca2cd127
Markus Heine <heine.m@gmxde>
2019-08-01 17:46:29 +0200

    add my own story. Have fun

--- text-adventure/index.js
+++ text-adventure/index.js
@@ -1,40 +1,81 @@
 const readline = require("readline");
 const chalk = require("chalk");
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+let name;
 
-const story = {
-    q: "Welcome to The Land Of Wizards! Would you like to play? ",
-    answers: {
-        yes: {
-            q:
-                "You are alone in a dark forest and facing a fork in the road. Which direction do you turn? ",
+function logName() {
+    rl.question(chalk.green("what is your name? "), answer => {
+        name = answer;
+        // console.log(name);
+        let story = {
+            q: `Hi ${name} do you want to go on a space odyssee? `,
             answers: {
-                left: {
-                    q:
-                        "There's a scary wizard! He asks you a tough question. What's 1+1? ",
+                yes: {
+                    q: `GREAT!! Do you want to follw me on board of the spaceship?`,
                     answers: {
-                        "2": "congratulations! "
+                        yes: {
+                            q:
+                                "You stand in front of the Captain. He asks you if you want do be a mechnaic or a pilot ",
+                            answers: {
+                                mechanic: {
+                                    q: `Alright! Your name is now Junior tech officer ${name}. Thats great do you want to start your first day of work? `,
+                                    answers: {
+                                        yes: {
+                                            q:
+                                                "there is a strange noise in the engine room. The captain said you should go down there and take a look. Will you do this? ",
+                                            answers: {
+                                                yes:
+                                                    "you find a intergalactic rat behind the flux capacitor. Unfortunaly the rat is very big and hungry. The rat starts to eat your delicate. YOU DIE!!!! ",
+                                                no:
+                                                    "you will be sentenced to death by an intergalactic military court for refusing to obey orders. YOU DIE!!"
+                                            }
+                                        },
+                                        no:
+                                            "you will be sentenced to death by an intergalactic military court for refusing to obey orders. YOU DIE!!"
+                                    }
+                                },
+                                pilot: {
+                                    q: `Tschooo Tschoo! Your name is now Junior Pilot ${name}. Thats great do you want to start your first day of work?  `,
+                                    answers: {
+                                        yes: {
+                                            q:
+                                                "The captain give you the order to go on Ludicrous Speed. Did you find the right button on your first day? ",
+                                            answers: {
+                                                yes:
+                                                    "Unfortunaly it is your first day and you forget to buckle up and you have to brake sharply because a intergalactic wall appears. YOU DIE!!!! ",
+                                                no:
+                                                    "you will be sentenced to death by an intergalactic military court for not knowing the basics. YOU DIE!!"
+                                            }
+                                        },
+                                        no:
+                                            "you will be sentenced to death by an intergalactic military court for refusing to obey orders. YOU DIE!!"
+                                    }
+                                }
+                            }
+                        },
+                        no: "no"
                     }
                 },
-                right: "This was not the right choice. Goodbye! "
+                no:
+                    "The spaceship starts imediately and you burn in the fire of the rocket engine. YOU DIE!!!"
             }
-        },
-        no: "Alright then. Enjoy your day! "
-    }
-};
+        };
+        askQuestion(story);
+    });
+}
 
 function askQuestion(storyObj) {
     rl.question(chalk.green(storyObj.q), answer => {
-        console.log(chalk.blue(answer));
-        console.log(typeof storyObj.answers[answer]);
+        // console.log(name);
+        // console.log(chalk.blue(answer));
+        // console.log(typeof storyObj.answers[answer]);
         if (typeof storyObj.answers[answer] == "object") {
-            console.log("++++++++++++++");
-            console.log(storyObj.answers[answer]);
-            console.log("++++++++++++++");
+            // console.log("++++++++++++++");
+            // console.log(storyObj.answers[answer]);
+            // console.log("++++++++++++++");
             askQuestion(storyObj.answers[answer]);
         } else if (typeof storyObj.answers[answer] == "string") {
             console.log(storyObj.answers[answer]);
@@ -46,4 +87,4 @@ function askQuestion(storyObj) {
     });
 }
 
-askQuestion(story);
+logName();
